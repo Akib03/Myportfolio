dab70546dd94c941ba2dc1f9a4091d25c6f30782
Mohd. Aqib <91658001+Akib03@users.noreply.github.com>
2022-07-04 15:23:31 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'portfolio' );
+define( 'DB_NAME', 'sql6503896' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'sql6503896' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'CiDxd2pcBh' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql6.freesqldatabase.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
